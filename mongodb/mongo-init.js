20dcb48cda9378e35fb14b88f1984d3e33de0e76
dluna88-sea <diego.luna@seacoahuila.org.mx>
2024-10-22 15:46:07 -0600

    ajuste al mongo-init

--- mongodb/mongo-init.js
+++ mongodb/mongo-init.js
@@ -1,30 +1,33 @@
+// Conectarse a la base de datos 'admin'
+db = db.getSiblingDB('admin');
+
+// Crear usuarios en la base de datos 'admin'
 db.createUser({
     user: 'pdnCoahOAuth',
     pwd: 'pdnc04h04u7h',
     roles: [
-      { role: 'readWrite', db: 's1-oauth' },
-      { role: 'dbAdmin', db: 's1-oauth' }
+        { role: 'readWrite', db: 's1-oauth' },
+        { role: 'dbAdmin', db: 's1-oauth' }
     ]
-  });
-  
-  db = db.getSiblingDB('s1-oauth'); // Cambiar a la base de datos s1-oauth
-  
-  db.oauths.insertOne({
+});
+
+db.createUser({
+    user: 'pdnCoah_api',
+    pwd: 'pdnc04h_api',
+    roles: [
+        { role: 'readWrite', db: 's1-api' },
+        { role: 'dbAdmin', db: 's1-api' }
+    ]
+});
+
+// Conectarse a la base de datos 's1-oauth'
+db = db.getSiblingDB('s1-oauth');
+
+// Insertar documento en la colección 'oauths'
+db.oauths.insertOne({
     "username": "pdnCoahOauth",
     "password": "pdnc04Oauth",
     "scope": ["read"],
     "client_id": "pdnCoahOauth",
     "client_secret": "53CR37"
-  });
-  
-  db = db.getSiblingDB('admin'); // Regresar a la base de datos admin
-  
-  db.createUser({
-    user: 'pdnCoah_api',
-    pwd: 'pdnc04h_api',
-    roles: [
-      { role: 'readWrite', db: 's1-api' },
-      { role: 'dbAdmin', db: 's1-api' }
-    ]
-  });
-  
+});
